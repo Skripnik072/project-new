64cda188804109f5df7824d35d0df4247bd926cb
Виктор Скрипник <viktor_ds@mail.ru>
2025-03-22 12:01:23 +0800

    fixed cod nou check

--- src/processing.py
+++ src/processing.py
@@ -8,6 +8,9 @@ def filter_by_state(my_list: list, state: str = 'EXECUTED') -> list:
     return new_list
 
 
+print(filter_by_state([{'id': 41428829, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'}], "EXECUTED"))
+
+
 def sort_by_date(my_list: list, reverse: bool = True) -> list:
     '''Функция сортирует список словарей по дате'''
     sorted_list = sorted(my_list, key=lambda x: x['date'], reverse=reverse)
