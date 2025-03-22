d1a091ee63ade0eaab4992a98025460f191edbfd
Виктор Скрипник <viktor_ds@mail.ru>
2025-03-22 11:56:40 +0800

    fixed cod user

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,12 @@ def filter_by_state(my_list: list, state: str = 'EXECUTED') -> list:
             if value == state:
                 new_list.append(dict)
     return new_list
+
+
+def sort_by_date(my_list: list, reverse: bool = True) -> list:
+    '''Функция сортирует список словарей по дате'''
+    sorted_list = sorted(my_list, key=lambda x: x['date'], reverse=reverse)
+    return sorted_list
+
+
+print()
