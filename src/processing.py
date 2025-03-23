d6ea720732672b8bb12c9d5340f386be9cafaf55
Виктор Скрипник <viktor_ds@mail.ru>
2025-03-23 09:11:45 +0800

    fixed hw101 ver2

--- src/processing.py
+++ src/processing.py
@@ -8,7 +8,7 @@ def filter_by_state(my_list: list, state: str = 'EXECUTED') -> list:
     return new_list
 
 
-print(filter_by_state([{'id': 41428829, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'}], "EXECUTED"))
+# print(filter_by_state([{'id': 41428829, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'}], "EXECUTED"))
 
 
 def sort_by_date(my_list: list, reverse: bool = True) -> list:
@@ -17,4 +17,4 @@ def sort_by_date(my_list: list, reverse: bool = True) -> list:
     return sorted_list
 
 
-print(sort_by_date([{'id': 41428829, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'}]))
+# print(sort_by_date([{'id': 41428829, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'}]))
